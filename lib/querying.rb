1e46172daa2182e3537227ba69377ada619a5327
Jenny Chu <jchudesign0930@gmail.com>
2019-10-18 21:10:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 
- def select_name_and_motto_of_char_with_longest_motto	def select_name_and_motto_of_char_with_longest_motto
+ def select_name_and_motto_of_char_with_longest_motto	
   "SELECT name, motto 
   FROM characters 
   ORDER BY LENGTH(motto) DESC 
@@ -14,7 +14,7 @@ end
 end
 
 
- def select_value_and_count_of_most_prolific_species	def select_value_and_count_of_most_prolific_species
+ def select_value_and_count_of_most_prolific_species	
   "SELECT species, COUNT(species) 
   FROM characters 
   GROUP BY species 
@@ -23,7 +23,7 @@ end
 end
 
 
- def select_name_and_series_subgenres_of_authors	def select_name_and_series_subgenres_of_authors
+ def select_name_and_series_subgenres_of_authors	
   "SELECT authors.name, subgenres.name
   FROM series
   INNER JOIN authors
@@ -33,7 +33,7 @@ end
 end
 
 
- def select_series_title_with_most_human_characters	def select_series_title_with_most_human_characters
+ def select_series_title_with_most_human_characters	
   "SELECT Series.title
   FROM characters
   INNER JOIN series
@@ -45,7 +45,7 @@ end
 end
 
 
- def select_character_names_and_number_of_books_they_are_in	def select_character_names_and_number_of_books_they_are_in
+ def select_character_names_and_number_of_books_they_are_in	
   "SELECT characters.name, COUNT(name)
   FROM character_books
   INNER JOIN characters
